df798f392a8ff04f46d07d845ca33a7fbc2c7dcd
estinbramka <estin1996@gmail.com>
2023-07-18 15:21:26 +0300

    added account linking with google

--- client/firebase/auth/signup.ts
+++ client/firebase/auth/signup.ts
@@ -1,5 +1,5 @@
 import firebase_app from "../config";
-import { EmailAuthProvider, UserCredential, createUserWithEmailAndPassword, getAuth, linkWithCredential } from "firebase/auth";
+import { EmailAuthProvider, GoogleAuthProvider, User, UserCredential, createUserWithEmailAndPassword, getAuth, linkWithCredential, linkWithPopup } from "firebase/auth";
 
 const auth = getAuth(firebase_app);
 
@@ -23,7 +23,23 @@ export async function signUpAnonymous(email: string, password: string): Promise<
         const credential = EmailAuthProvider.credential(email, password);
         if (auth.currentUser !== null) {
             result = await linkWithCredential(auth.currentUser, credential);
-        }else{
+        } else {
+            error = 'Guest account not found'
+        }
+    } catch (e) {
+        error = e;
+    }
+
+    return { result, error };
+}
+
+export async function signUpGoogleUpgrade(): Promise<{ result: UserCredential | null, error: any }> {
+    let result = null,
+        error = null;
+    try {
+        if (auth.currentUser !== null) {
+            result = await linkWithPopup(auth.currentUser, new GoogleAuthProvider)
+        } else {
             error = 'Guest account not found'
         }
     } catch (e) {
