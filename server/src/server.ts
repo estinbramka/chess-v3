51f446faea101d1470b247893aabcc3cb837a618
estinbramka <estin1996@gmail.com>
2023-07-18 16:19:03 +0300

    change port number

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,11 @@
 import express, { Request, Response } from "express";
 const app = express()
-const port = 5000
+const PORT = process.env.PORT || 4000;
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!')
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`)
 })
