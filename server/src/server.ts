7cd3517f76544f1e89ef94f72e040d973dd70ee2
estinbramka <estin1996@gmail.com>
2023-07-18 16:12:00 +0300

    added types on server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,11 @@
-const express = require('express')
+import express, { Request, Response } from "express";
 const app = express()
 const port = 5000
 
-app.get('/', (req:any, res:any) => {
-  res.send('Hello World!')
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
