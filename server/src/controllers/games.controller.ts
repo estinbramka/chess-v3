346c88a0f003c8cfb6eb7fe8ffe4519ca59f3c8a
estinbramka <estin1996@gmail.com>
2023-09-06 11:45:31 +0300

    redeploy

--- server/src/controllers/games.controller.ts
+++ server/src/controllers/games.controller.ts
@@ -2,5 +2,5 @@ import type { Request, Response } from "express";
 
 
 export const helloWorld = async (req: Request, res: Response) => {
-    res.status(200).json({msg:"hello world from game controller redeployed"});
+    res.status(200).json({msg:"hello world from game controller"});
 };
